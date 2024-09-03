9f8628bb4ee47c9ed1787caf2d0a8456e7ede32d
kaminenileela <kaminenileela1992@email.com>
2024-09-02 20:45:57 -0400

    expense

--- terraform/frontend.sh
+++ terraform/frontend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-component=$1
-environment=$2
-app_version=$3
-dnf install ansible -y
-pip 3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/kaminenileela/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment -e appVersion=$app_version
+component=$1;
+environment=$2;
+app_version=$3;
+dnf install ansible -y;
+pip3.9 install botocore boto3;
+ansible-pull -i localhost, -U https://github.com/kaminenileela/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment -e appVersion=$app_version;
